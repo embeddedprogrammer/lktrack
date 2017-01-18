6fd10c616fb7a77c54130ed8b658e829db2dddb9
Unknown <snowflakeobsidian3.14@gmail.com>
2017-01-18 16:44:00 -0700

    Overlay image at particular location.

--- gen_frame.py
+++ gen_frame.py
@@ -9,21 +9,31 @@ key_right = 83
 key_down = 84
 
 # Load images
-img_bg = cv2.imread("imgs/forest.jpg")
-img_fg = cv2.imread("imgs/bat.png", cv2.IMREAD_UNCHANGED)
-img_bg = img_bg[:275, :494, :3]
+bg = cv2.imread("imgs/forest.jpg")
+fg = cv2.imread("imgs/bat.png", cv2.IMREAD_UNCHANGED)
+#bg = bg[:275, :494, :3]
 
 def overlayImg(bg, fg):
     alpha = fg[:, :, 3:4] / 255.
     return ((1 - alpha)*bg[:, :, :3] + alpha*fg[:, :, :3]).astype(np.uint8)
 
-img = overlayImg(img_bg, img_fg)
-cv2.imshow('bg', img_bg)
-cv2.imshow('fg', img_fg)
+def overlay2(bg, fg, pt_fg, pt_bg):
+    down = np.array([[1., 0.]], dtype=np.float32)
+    right = np.array([[0., 1.]], dtype=np.float32)
+    pts_bg = np.stack([pt_bg, pt_bg + down, pt_bg + right + down, pt_bg + right], axis=0)
+    pts_fg = np.stack([pt_fg, pt_fg + down, pt_fg + right + down, pt_fg + right], axis=0)
+    T = cv2.getPerspectiveTransform(pts_fg, pts_bg)
+    fg_warped = cv2.warpPerspective(fg, T, (bg.shape[1], bg.shape[0]))
+    return overlayImg(bg, fg_warped)
+
+img = overlay2(bg, fg, np.array([[0., 0.]], dtype=np.float32), np.array([[100., 0.]], dtype=np.float32))
+
+cv2.imshow('bg', bg)
+cv2.imshow('fg', fg)
 cv2.imshow('image', img)
 while True:
     key = cv2.waitKey(0) & 0xff
-    if key == key_esc:
+    if key == key_esc or key == ord('q'):
         break
     else:
         print key
