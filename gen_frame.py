80fc0852c969972fd624120d9b94df7635563518
Unknown <snowflakeobsidian3.14@gmail.com>
2017-01-19 10:26:59 -0700

    Add LK tracker.

--- gen_frame.py
+++ gen_frame.py
@@ -1,6 +1,7 @@
 import numpy as np
 import cv2
 import time
+from vanilla_lk import LK
 
 key_esc = 27
 key_esc = 27
@@ -12,15 +13,22 @@ key_down = 84
 # Load images
 bg = cv2.imread("imgs/forest.jpg")
 fg = cv2.imread("imgs/bat.png", cv2.IMREAD_UNCHANGED)
-#bg = bg[:275, :494, :3]
+
+fg = cv2.resize(fg, (0, 0), fx=.3, fy=.3)
+bg = cv2.resize(bg, (0, 0), fx=.3, fy=.3)
+
+global times
+times = []
 
 def tic():
-    global t0
-    t0 = time.time()
+    times.append(time.time())
 
 def toc(name = ""):
-    t = time.time() - t0
-    print "%s: %.0f ms" %(name, t * 1000)
+    if len(times) == 0:
+        print "Must call tic before toc"
+    else:
+        t = time.time() - times.pop()
+        print "%s: %.0f ms" %(name, t * 1000)
 
 def overlayImg(bg, fg):
     alpha = fg[:, :, 3:4] / 255.
@@ -35,14 +43,19 @@ def overlay2(bg, fg, pt_fg, pt_bg, scale):
     pts_bg = np.stack([pt_bg, pt_bg + down, pt_bg + right + down, pt_bg + right], axis=0)
     T = cv2.getPerspectiveTransform(pts_fg, pts_bg)
     fg_warped = cv2.warpPerspective(fg, T, (bg.shape[1], bg.shape[0]))
-    return overlayImg(bg, fg_warped)
+    img = overlayImg(bg, fg_warped)
+    return img
+
+def process(x, y):
+    img = overlay2(bg, fg, np.array([[fg.shape[1] / 2, fg.shape[0] / 2]], dtype=np.float32), np.array([[x, y]], dtype=np.float32), scale=.3)
+    cv2.imshow('image', img)
 
 x = 100.
 y = 100.
 dist = 10.
 
 while True:
-    key = cv2.waitKey(-1) & 0xff
+    key = cv2.waitKey(0) & 0xff
     if key == key_esc or key == ord('q'):
         break
     elif key == key_left:
@@ -55,10 +68,7 @@ while True:
         y += dist
     else:
         print key
-    tic()
-    img = overlay2(bg, fg, np.array([[fg.shape[1] / 2, fg.shape[0] / 2]], dtype=np.float32), np.array([[x, y]], dtype=np.float32), scale=.1)
-    toc("Total")
-    cv2.imshow('image', img)
+    drawBgFg(x, y)
 
 cv2.destroyAllWindows()
 
---
+++ lk_tracker.py
@@ -0,0 +1,153 @@
+import numpy as np
+import cv2
+
+class LK:
+    def __init__(self, method='custom', gradientMethod='scharr', winSize=21, count=10, err=1e-4, maxLevel=0):
+        self.method = method
+        self.gradientMethod = gradientMethod
+        self.count = count
+        self.err = err
+        self.winSize = winSize
+        self.maxLevel = maxLevel
+        self.lastImg = None
+
+    @staticmethod
+    def interpolate(img, x, y):
+        ix = int(x)
+        iy = int(y)
+        fx = x - ix
+        fy = y - iy
+        p00 = img[iy, ix]
+        p01 = img[iy, ix + 1]
+        p10 = img[iy + 1, ix]
+        p11 = img[iy + 1, ix + 1]
+        return (1 - fy) * (1 - fx) * p00 + (1 - fy) * fx * p01 + fy * (1 - fx) * p10 + fy * fx * p11
+
+    @staticmethod
+    def cropImg(img, cx, cy, winSize, fullSize=False):
+        assert winSize % 2 == 1
+        border = int((winSize - 1) / 2)
+        if fullSize:
+            newImg = np.zeros_like(img)
+        elif len(img.shape) == 3:
+            newImg = np.zeros((winSize, winSize, img.shape[2]))
+        else:
+            newImg = np.zeros((winSize, winSize))
+        for y in range(-border, border + 1):
+            for x in range(-border, border + 1):
+                newImg[border + y, border + x] = LK.interpolate(img, cx + x, cy + y)
+        return newImg
+
+    @staticmethod
+    def drawHatch(img, cx, cy, clr=[255, 0, 0], hatch_size=2):
+        img[cx - hatch_size: cx + hatch_size + 1, cy] = np.array(clr)
+        img[cx, cy - hatch_size: cy + hatch_size + 1] = np.array(clr)
+
+    @staticmethod
+    def fillRect(img, x0, x1, y0, y1, c):
+        img[y0:y1 + 1, x0:x1 + 1] = c
+
+    @staticmethod
+    def drawRect(img, x0, x1, y0, y1, c=[255, 0, 0]):
+        print x0, x1, y0, y1
+        LK.fillRect(img, x0, x0, y0, y1, c)
+        LK.fillRect(img, x1, x1, y0, y1, c)
+        LK.fillRect(img, x0, x1, y0, y0, c)
+        LK.fillRect(img, x0, x1, y1, y1, c)
+
+    # Note: scipy.misc.imresize has a weird rounding problem. This makes
+    # it not as nice for using 'nearest' interpolation, so we created our
+    # own function
+    @staticmethod
+    def scale(img, factor):
+        newImg = np.zeros((img.shape[0] * factor, img.shape[1] * factor, img.shape[2]))
+        for y in range(img.shape[0]):
+            for x in range(img.shape[1]):
+                newImg[y * factor:(y + 1) * factor, x * factor:(x + 1) * factor] = img[y, x]
+        return newImg
+
+    @staticmethod
+    def showLarge(img, factor=9, draw=False):
+        cx = int((img.shape[0] - 1) / 2) * factor
+        cy = int((img.shape[1] - 1) / 2) * factor
+        img = LK.scale(img, factor)
+        print img.shape
+        # Draw cross hatch
+        # drawHatch(img, cx + (factor-1)/2, cy + (factor-1)/2)
+        LK.drawRect(img, cx, cx + factor - 1, cy, cy + factor - 1)
+        if draw:
+            misc.imshow(img)
+        else:
+            return img
+
+    @staticmethod
+    def combine(imgs, draw=True):
+        img = np.concatenate(imgs, 1)
+        if draw:
+            misc.imshow(img)
+        else:
+            return img
+
+    @staticmethod
+    def getGradient(img, method='cv_scharr'):
+        if method == 'cv_scharr':
+            gradx = cv2.Scharr(img, cv2.CV_64F, 1, 0) / 32
+            grady = cv2.Scharr(img, cv2.CV_64F, 0, 1) / 32
+            return gradx, grady
+
+        if method == 'simple':
+            kernel = np.array([[-1, 0, 1]], dtype='float') / 2
+        elif method == 'sobel':
+            kernel = np.array([[-1, 0, 1],
+                               [-2, 0, 2],
+                               [-1, 0, 1]], dtype='float') / 8
+        elif method == 'scharr':
+            kernel = np.array([[-3, 0, 3],
+                               [-10, 0, 10],
+                               [-3, 0, 3]], dtype='float') / 32
+
+        gradx = cv2.filter2D(img, -1, kernel)
+        grady = cv2.filter2D(img, -1, kernel.T)
+        return gradx, grady
+
+    def track(self, img):
+        if self.lastImg is None:
+            self.lastImg = img
+            return
+
+        if self.method == 'custom':
+            # Extract template image patch
+            template = LK.cropImg(self.lastImg, self.x, self.y, self.winSize)
+            dx, dy = LK.getGradient(template, method=self.gradientMethod)
+
+            # Precompute jacobian and inverse of grammian (these are constant)
+            J = np.concatenate((np.reshape(dx, (-1, 1)), np.reshape(dy, (-1, 1))), axis=1)
+            G_inv = np.linalg.inv(np.matmul(J.T, J))
+
+            # Iterative gauss-newton algorithm
+            for i in range(self.count):
+                # Extract patch from current image and compute error
+                croppedImg = LK.cropImg(img, self.x, self.y, 21)
+                residual = np.reshape(template - croppedImg, (-1, 1))
+                error = np.linalg.norm(residual)
+                if error < self.err:
+                    print "Iter %d Terminate at err: %.3f" %(i, error)
+                    break
+
+                # Compute least-squares solution to linearized equation
+                deltaX = np.matmul(G_inv, np.matmul(J.T, residual))
+                self.x += deltaX[0, 0]
+                self.y += deltaX[1, 0]
+                print "Iter %d err: %.3f   lk: (%.3f, %.3f)" % (i, error, self.x, self.y)
+
+        elif self.method == 'cv':
+            lk_params = dict(winSize=self.winSize,
+                             maxLevel=self.maxLevel,
+                             criteria=(cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, self.count, self.err))
+            p0 = np.array([[[self.x, self.y]]], dtype='float32')
+            p1, st, err = cv2.calcOpticalFlowPyrLK(self.lastImg, img, p0, **lk_params)
+            self.x = p1[0, 0, 0]
+            self.y = p1[0, 0, 1]
+
+        self.lastImg = img
+        return self.x, self.y
