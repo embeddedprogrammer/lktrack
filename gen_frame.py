7dd93c35a96c59d5388a3e7795a931c62d25b373
Unknown <snowflakeobsidian3.14@gmail.com>
2017-01-18 20:02:14 -0700

    Time code.

--- gen_frame.py
+++ gen_frame.py
@@ -1,5 +1,6 @@
 import numpy as np
 import cv2
+import time
 
 key_esc = 27
 key_esc = 27
@@ -13,30 +14,52 @@ bg = cv2.imread("imgs/forest.jpg")
 fg = cv2.imread("imgs/bat.png", cv2.IMREAD_UNCHANGED)
 #bg = bg[:275, :494, :3]
 
+def tic():
+    global t0
+    t0 = time.time()
+
+def toc(name = ""):
+    t = time.time() - t0
+    print "%s: %.0f ms" %(name, t * 1000)
+
 def overlayImg(bg, fg):
     alpha = fg[:, :, 3:4] / 255.
     return ((1 - alpha)*bg[:, :, :3] + alpha*fg[:, :, :3]).astype(np.uint8)
 
-def overlay2(bg, fg, pt_fg, pt_bg):
+def overlay2(bg, fg, pt_fg, pt_bg, scale):
     down = np.array([[1., 0.]], dtype=np.float32)
     right = np.array([[0., 1.]], dtype=np.float32)
-    pts_bg = np.stack([pt_bg, pt_bg + down, pt_bg + right + down, pt_bg + right], axis=0)
     pts_fg = np.stack([pt_fg, pt_fg + down, pt_fg + right + down, pt_fg + right], axis=0)
+    down *= scale
+    right *= scale
+    pts_bg = np.stack([pt_bg, pt_bg + down, pt_bg + right + down, pt_bg + right], axis=0)
     T = cv2.getPerspectiveTransform(pts_fg, pts_bg)
     fg_warped = cv2.warpPerspective(fg, T, (bg.shape[1], bg.shape[0]))
     return overlayImg(bg, fg_warped)
 
-img = overlay2(bg, fg, np.array([[0., 0.]], dtype=np.float32), np.array([[100., 0.]], dtype=np.float32))
+x = 100.
+y = 100.
+dist = 10.
 
-cv2.imshow('bg', bg)
-cv2.imshow('fg', fg)
-cv2.imshow('image', img)
 while True:
-    key = cv2.waitKey(0) & 0xff
+    key = cv2.waitKey(-1) & 0xff
     if key == key_esc or key == ord('q'):
         break
+    elif key == key_left:
+        x -= dist
+    elif key == key_right:
+        x += dist
+    elif key == key_up:
+        y -= dist
+    elif key == key_down:
+        y += dist
     else:
         print key
+    tic()
+    img = overlay2(bg, fg, np.array([[fg.shape[1] / 2, fg.shape[0] / 2]], dtype=np.float32), np.array([[x, y]], dtype=np.float32), scale=.1)
+    toc("Total")
+    cv2.imshow('image', img)
+
 cv2.destroyAllWindows()
 
 
